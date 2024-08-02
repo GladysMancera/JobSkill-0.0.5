d9ff8806e8db7fa79ca7403cd9ce55616c39b1f4
Gladys Mancera <g.gladysmancera@gmail.com>
2024-08-02 13:54:28 -0600

    Nuevas plantillas en app empresa

--- paginaEmpresa/views.py
+++ paginaEmpresa/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "paginaEmpresa/home.html")
+
+def agregar(request):
+    return render(request, "paginaEmpresa/agregar.html")
+
+def editar(request):
+    return render(request, "paginaEmpresa/editar.html")
+
+def perfil(request):
+    return render(request, "paginaEmpresa/perfil.html")
