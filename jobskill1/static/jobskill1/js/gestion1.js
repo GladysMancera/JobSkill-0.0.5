1fabe8a239f18bc2c9796d03b3ca3f5ad054870e
Raul Enrique Mendoza Ramirez <mendozaramirezraulenrique@gmail.com>
2024-08-05 00:33:15 -0600

    Login validacion

---
+++ jobskill1/static/jobskill1/js/gestion1.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // VALIDACION DE LOS CAMPOS NOMBRE Y CONTRASEÑA
+
+
+    const form = document.querySelector("form");
+    const username = document.getElementById("nombre");
+    const password = document.getElementById("password");
+
+    form.addEventListener("submit", function(event) {
+        
+        if (username.value.trim() === "") {
+
+            alert("El nombre de usuario no puede estar vacío.");
+            event.preventDefault();
+            return;
+        }
+
+        if (password.value.trim() === "") {
+            alert("La contraseña no puede estar vacía.");
+            event.preventDefault();
+            return;
+            // ENVIO DE ALERTAS EN CASO DE DEJARLOS EN BLANCO
+        }
+ 
+        // ESPACIO PARA COLOCAR MAS VALIDACIONES
+    });
+});
--- paginaUsuario/models.py
+++ paginaUsuario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=10)
+    email = models.EmailField(max_length=30)
+    apellido = models.CharField(max_length=20)
+    genero = models.CharField(max_length=15)
+    fecha_nacimiento = models.DateField()
+    password1 = models.CharField(max_length=8)  
+    password2 = models.CharField(max_length=8)  
